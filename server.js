4cb1855e2338e2e9abe47fda0f9aa58393ee1980
Andria-Hibe <andriacohibe@gmail.com>
2020-03-27 19:52:45 +1300

    Parse req body as urlencoded

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-const githubURL = 'https://api.github.com/repos/hotosm/tasking-manager/issues?state=open;labels=Difficulty:%20Easy'
+const githubURL = 'https://api.github.com/repos/hotosm/tasking-manager/issues?state=open;labels=Difficulty:%20Hard'
+const responseURL = 'https://hooks.slack.com/commands/T010DJXL4GM/1030203970213/jU4tbGu3wIRhXOVCQzPABpWz'
 
-app.use(express.json())
+app.use(bodyParser.urlencoded())
 
-
-// Route for all open issues with Difficulty:Easy label
+// Route for all open issues with Difficulty:Hard label right now - need to be Easy
 app.post('/api/github', async (req, res) => {  
+  console.log(req.body)
+
   const githubResponse = await fetch(githubURL)
   const githubJSON = await githubResponse.json()
 
@@ -20,7 +23,7 @@ app.post('/api/github', async (req, res) => {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: `${issue.number} - <${issue.url}|*${issue.title}*>`
+            text: `#${issue.number} - <${issue.url}|*${issue.title}*>`
           }
         },
         {
