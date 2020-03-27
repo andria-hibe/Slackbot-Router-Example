2e1d3ef87813d50004ed8f1bff13173b57978b91
Andria-Hibe <andriacohibe@gmail.com>
2020-03-27 19:57:54 +1300

    Get response URL from body of HTTP request

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const bodyParser = require('body-parser')
 const app = express()
 
 const githubURL = 'https://api.github.com/repos/hotosm/tasking-manager/issues?state=open;labels=Difficulty:%20Hard'
-const responseURL = 'https://hooks.slack.com/commands/T010DJXL4GM/1030203970213/jU4tbGu3wIRhXOVCQzPABpWz'
 
 app.use(bodyParser.urlencoded())
 
 // Route for all open issues with Difficulty:Hard label right now - need to be Easy
 app.post('/api/github', async (req, res) => {  
-  console.log(req.body)
-
+  const responseURL = req.body.response_url
+  
   const githubResponse = await fetch(githubURL)
   const githubJSON = await githubResponse.json()
 
