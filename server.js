10d4b9e2d924bb90e31639a04a7775d6afca5671
Andria-Hibe <andriacohibe@gmail.com>
2020-03-28 14:25:07 +1300

    Refactor code

--- server.js
+++ server.js
@@ -3,15 +3,34 @@ const fetch = require('node-fetch')
 
 const app = express()
 
-const githubURL = 'https://api.github.com/repos/hotosm/tasking-manager/issues?state=open;labels=Difficulty:%20Hard'
+const GITHUB_URL = 'https://api.github.com/repos/hotosm/tasking-manager/issues?state=open;labels=Difficulty:%20Hard'
 
 app.use(express.urlencoded({ extended: true }))
+
+function groupIssuesIntoMessages (array, size) {
+  const blocksArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    const lastBlock = blocksArray[blocksArray.length - 1]
+    
+    if (!lastBlock || lastBlock.length === size) {
+      blocksArray.push([array[i]])
+    } else {
+      lastBlock.push(array[i])
+    }
+  }
+  return blocksArray.map(block => {
+    return {
+      blocks: block
+    }
+  })
+}
 
 // Route for all open issues with Difficulty:Hard label right now - need to be Easy
 app.post('/api/github', async (req, res) => {  
   const responseURL = req.body.response_url
 
-  const githubResponse = await fetch(githubURL)
+  const githubResponse = await fetch(GITHUB_URL)
   const githubJSON = await githubResponse.json()
 
   const issuesArray = githubJSON.reduce(
@@ -40,48 +59,20 @@ app.post('/api/github', async (req, res) => {
       return accumulator
     }, []
   )
+  
+  const BLOCK_COUNT = 9
+  const slackMessages = groupIssuesIntoMessages(issuesArray, BLOCK_COUNT)
 
-
-  function slackBlocks (array, size) {
-    const blocksArray = []
-
-    for (let i = 0; i < array.length; i++) {
-      const lastBlock = blocksArray[blocksArray.length - 1]
-      
-      if (!lastBlock || lastBlock.length === size) {
-        blocksArray.push([array[i]])
-      } else {
-        lastBlock.push(array[i])
-      }
-    }
-    return blocksArray
-  }
-
-  const slackMessages = slackBlocks(issuesArray, 9)
-
-  const sendMessage = (message) => {
-    return {
-      blocks: message
-    }
-  }
-
-  const firstMessage = sendMessage(slackMessages[0])
+  const firstMessage = slackMessages[0]
   res.json(firstMessage)
 
-  Promise.all(slackMessages.slice(1).map(message => {
-    const nextMessage = sendMessage(message)
-
+  slackMessages.slice(1).map(message => {
     fetch(responseURL, {
       method: 'post',
-      body: JSON.stringify(nextMessage),
+      body: JSON.stringify(message),
       headers: {'Content-Type': 'application/json'}
     })
-      .then(res => res.json())
-
-    // fetch(responseURL, { method: 'POST', body: 'a=1' })
-    //   .then(res => res.json(nextMessage))
-    //   .then(json => console.log(json))
-  }))
+  })
 })
 
 module.exports = app
